5fec4e9e2e3ce43ee5c560d4c2f1256cc02f179c
Milton-C-Ribeiro <milton.carlos.java@gmail.com>
2024-03-17 06:20:13 +0900

    alteração da clase

---
+++ primeiro-programa-java/src/Miltonmeu/Pessoa.java
@@ -0,0 +1,27 @@
+package Miltonmeu;
+
+public class Pessoa {
+
+		private String nome;
+		
+		private string sobrenome;
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public string getSobrenome() {
+			return sobrenome;
+		}
+
+		public void setSobrenome(string sobrenome) {
+			this.sobrenome = sobrenome;
+		}
+		
+		
+		
+}
