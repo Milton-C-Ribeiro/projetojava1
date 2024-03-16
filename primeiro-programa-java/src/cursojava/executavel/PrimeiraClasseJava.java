98c5eea5a24a66840b464035e15e951ac064849c
Milton <milton.carlos.dev@gmail.com>
2024-03-17 04:43:20 +0900

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*main é um metodo auto executavel em java, seu atalho é main + CTRL e SPACE juntos*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("meu primeiro programa em Java");
+		System.out.println("oi");
+	}
+}
